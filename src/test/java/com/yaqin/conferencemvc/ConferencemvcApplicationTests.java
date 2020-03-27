f9218fa83283c9c8a0c4db5e100b09d7d6c74024
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-03-27 14:41:03 -0600

    initial commit

---
+++ src/test/java/com/yaqin/conferencemvc/ConferencemvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaqin.conferencemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencemvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
